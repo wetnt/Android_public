4bd1a91e147e00abdeec09764257ebebfbce6bcd
wetnt <wetnt@sina.com>
2015-04-14 13:30:43 +0800

    test3x

--- BBK_Test2/src/com/example/bbk_test2/MainActivity.java
+++ BBK_Test2/src/com/example/bbk_test2/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends Activity {
     }
 
 
+    public void test(){
+    	int x = 0;
+    }
+    
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
